6ffca20a2a487b5e44b4d1691cd468c0d9dea2d3
nuellyprecious96 <anyanwuemman178@gmail.com>
2026-05-11 08:24:40 -0400

    update

--- jsarraysort.js
+++ jsarraysort.js
@@ -1,53 +1,53 @@
-// // // Arranging in ascending or descending order
-// // // ascending => small to big
-// // // descending => big to small
+// // // // Arranging in ascending or descending order
+// // // // ascending => small to big
+// // // // descending => big to small
 
-// // // .sort() we use this method
+// // // // .sort() we use this method
 
-// // // Sorting string arrays
-// const fruits = ["Banana", "Apple", "Mango", "Pear"] 
-//console.log(fruits.sort())
+// // // // Sorting string arrays
+// // const fruits = ["Banana", "Apple", "Mango", "Pear"] 
+// //console.log(fruits.sort())
 
-// // // // Reverse a  string array
-// // // // we use the reverse() function
-//console.log(fruits.reverse())
+// // // // // Reverse a  string array
+// // // // // we use the reverse() function
+// //console.log(fruits.reverse())
 
-// // Palindrome
-// // A string that reads same fowards and backwards
-pal = 'Civic'
-palArray = new String (pal)
+// // // Palindrome
+// // // A string that reads same fowards and backwards
+// pal = 'Civic'
+// palArray = new String (pal)
 
 
-//console.log(palArray)
+// //console.log(palArray)
 
-// // // var reverse
-reverse = 'Civic'
-if (pal == reverse) {
-    console.log('Yes, it\'s a palindrome')
-} else {
-console.log('No, it\'s not a palindrome')
-}
+// // // // var reverse
+// reverse = 'Civic'
+// if (pal == reverse) {
+//     console.log('Yes, it\'s a palindrome')
+// } else {
+// console.log('No, it\'s not a palindrome')
+// }
 
-// // // Numeric Sort
-const numbers = [56, 12, 10, 3]
-// // // we use the compare function when it comes 
-// // // to numbers
-function (a, b) {
-    return a - b
-}
+// // // // Numeric Sort
+// const numbers = [56, 12, 10, 3]
+// // // // we use the compare function when it comes 
+// // // // to numbers
+// function (a, b) {
+//     return a - b
+// }
 
-// // // if a - b gives a postive number
-// // // for example 3 - 2 = 1
-// // // that means a > b  
+// // // // if a - b gives a postive number
+// // // // for example 3 - 2 = 1
+// // // // that means a > b  
 
-// // // if a - b gives a negative number
-// // // for example 2 - 4 = -2
-// // // that means a < b
+// // // // if a - b gives a negative number
+// // // // for example 2 - 4 = -2
+// // // // that means a < b
 
-console.log(numbers.sort(function (a, b) {
-// //     return a - b
-// // })) // sorts in ascending order
+// console.log(numbers.sort(function (a, b) {
+// // //     return a - b
+// // // })) // sorts in ascending order
 
-// // console.log(numbers.sort(function (a, b) {
-// //     return b - a
-// // }))   // sorts in descending order
+// // // console.log(numbers.sort(function (a, b) {
+// // //     return b - a
+// // // }))   // sorts in descending order
